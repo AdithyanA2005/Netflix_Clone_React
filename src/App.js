096381bd667fd4ababe177a842a9cec8e069f65a
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-26 14:00:35 +0530

    Basic files added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import Navbar from './Components/Navbar/Navbar';
+import Rowpost from './Components/Rowpost/Rowpost';
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Banner />
+            <Rowpost />
+        </div>
+    );
+}
+
+export default App;
+
