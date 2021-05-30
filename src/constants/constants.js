9dca4a223a3f8a5a9fe003e345f9e9f5c18c4d4c
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-30 09:22:40 +0530

    Contants added

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+// <<ENTER YOUR API KEY BELOW>> 
+export const API_KEY = '';
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
+export const baseUrl = 'https://api.themoviedb.org/3/';
