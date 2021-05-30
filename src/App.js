9afbadab839e3170754c5f51d4c2447211ba6153
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-30 09:09:26 +0530

    App.js Updated
    App.js is now optimised and organised for routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
-import Rowpost from './Components/Rowpost/Rowpost';
+import Home from './Container/Home';
+import Movies from './Container/Movies';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <Banner />
-            <Rowpost />
+            <div className="container">
+                <Banner />
+                <Router>
+                    <Route component={Home} path="/home"></Route>
+                    <Route component={Movies} path="/movies"></Route>
+                </Router>
+            </div>
         </div>
     );
 }
