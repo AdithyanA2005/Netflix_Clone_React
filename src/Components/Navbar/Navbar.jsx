e8c2cad67d759c875982e0d464c8227716140182
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-30 09:08:25 +0530

    Navbar is updated for routes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,12 @@ function Navbar() {
     return (
         <nav className="navbar">
             <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix-Logo" />
+            <ul className="navitems">
+                <li><a className="navitem" href="/home">Home</a></li>
+                <li><a className="navitem" href="/movies">Movies</a></li>
+            </ul>
             <img className="avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Nav-Avatar" />
         </nav>
     )
 }
-
 export default Navbar
